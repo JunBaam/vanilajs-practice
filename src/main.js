ab9e042ec2a5c92ce2dc9b46bb0b3af5f57308ef
fkdlsl9088 <fkdlsl9088@naver.com>
2023-10-07 17:48:00 +0900

    :safety_vest: Add sumAllCounts

--- src/main.js
+++ src/main.js
@@ -24,29 +24,38 @@ function findElement(startingElement, selector) {
     }
     currentElement = currentElement.parentElement;
   }
+
   return null;
+}
+
+function sumAllCounts(countMap) {
+  const getTotalCount = Object.values(countMap).reduce((total, current) => {
+    total += current;
+    return total;
+  }, 0);
+
+  return getTotalCount;
 }
 
 async function main() {
   const products = await getProducts();
+  const countMap = {};
 
   document.querySelector("#products").innerHTML = products
     .map(
       (product, index) => `
-  <div class="product" data-product-id="${product.id}" data-product-index="${index}">
-  <img src="${product.images[0]}" alt="Images of ${product.name}"/>
-  <p>${product.name}</p>
-  </div>
-  <div>
-  <div class="flex items-center justify-between">
-<span>Price: ${product.regularPrice} <span>
-<div>
-<button type="button" class="btn-decrease bg-green-200 py-1 px-3 rounded-full text-green-800 hovder:bg-green-300">-</button>
-<span class="text-green-800 hidden"> 3</span>
-<button type="button" class="btn-increase bg-green-200 py-1 px-3 rounded-full text-green-800 hovder:bg-green-300">+</button>
+<div class="product" data-product-id="${product.id}" data-product-index="${index}">
+      <img src="${product.images[0]}" alt="Images of ${product.name}"/>
+      <p>${product.name}</p>
+    <div class="flex items-center justify-between">
+        <span>Price: ${product.regularPrice} <span>
+      <div>
+        <button type="button"  class="btn-decrease disabled:opacity-50 disabled:cursor-not-allowed  bg-green-200 py-1 px-3 rounded-full text-green-800 hovder:bg-green-300">-</button>
+        <span class="cart-count text-green-800"></span>
+        <button type="button" class="btn-increase bg-green-200 py-1 px-3 rounded-full text-green-800 hovder:bg-green-300">+</button>
+      </div>
+    </div>
 </div>
-</div>
-  </div>
 `
     )
     .join("");
@@ -57,12 +66,29 @@ async function main() {
     const productId = productElement.getAttribute("data-product-id");
     const productIndex = productElement.getAttribute("data-product-index");
     const product = products[productIndex];
-    console.log("product", product);
 
-    if (targetElement.matches(".btn-decrease")) {
-      console.log("감소");
-    } else if (targetElement.matches(".btn-increase")) {
-      console.log("증가");
+    if (countMap[productId] === undefined) {
+      countMap[productId] = 0;
+    }
+
+    if (
+      targetElement.matches(".btn-decrease") ||
+      targetElement.matches(".btn-increase")
+    ) {
+      if (targetElement.matches(".btn-decrease")) {
+        countMap[productId] -= 1;
+      } else if (targetElement.matches(".btn-increase")) {
+        countMap[productId] += 1;
+      }
+      const cartCount = productElement.querySelector(".cart-count");
+      cartCount.innerHTML = countMap[productId];
+      if (countMap[productId] === 0) {
+        cartCount.innerHTML = "";
+      }
+
+      document.querySelector(".total_count").innerHTML = `(${sumAllCounts(
+        countMap
+      )})`;
     }
   });
 }
